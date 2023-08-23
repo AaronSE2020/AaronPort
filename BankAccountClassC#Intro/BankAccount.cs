b0895f09c586373561ed5201e32ce1f568855475
AaronSE2020 <aaronjking0408@outlook.com>
2023-08-22 20:32:49 -0500

    BankAccount Class Project

---
+++ BankAccountClassC#Intro/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountClassC_Intro
+{
+    internal class BankAccount
+    {
+        private int accountNumber;
+        private string accountHolderName;
+        private double balance;
+
+        public BankAccount(int accountNumber, string accountHolderName, double initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.accountHolderName = accountHolderName;
+            this.balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"{amount:C} deposited. New balance: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposited amount.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount > 0 && amount <= balance) 
+            {
+                balance -= amount;
+                Console.WriteLine($"{amount:C} withdrawn. New balance: {balance:C}");
+            }
+            else 
+            {
+                Console.WriteLine("invalid withdrawn amount or insufficient balance.");
+            }
+        }
+
+        public void DisplayAccountInfo() 
+        {
+            Console.WriteLine($"Account Number: {accountNumber}");
+            Console.WriteLine($"Account Number: {accountHolderName}");
+            Console.WriteLine($"Current Balance: {balance:C}");
+        }
+    }
+}
---
+++ BankAccountClassC#Intro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BankAccount;
+
+class Program 
+{
+
+   static void Main(string[] args) 
+   {
+        BankAccount account = new BankAccount(123456, "John Doe", 1000.00);
+
+        Console.WriteLine("Welcome to the Bank Account System!");
+        account.DisplayAccountInfo();
+
+        while (true) 
+        {
+            Console.WriteLine("\nChoose an action:");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Display Account Info");
+            Console.WriteLine("4. Exit");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice) 
+            {
+                case 1: Console.WriteLine("Enter deposit amount: ");
+                        double depositAmount = Convert.ToDouble(Console.ReadLine());
+                        account.Deposit(depositAmount);
+                        break;
+                case 2: Console.WriteLine("Enter withdrawl amount: ");
+                        double withdrawalAmount = Convert.ToDouble(Console.ReadLine());
+                        account.Withdrawal(withdrawalAmount);
+                        break;
+                case 3: account.DisplayAccountInfo();
+                        break;
+                case 4: Console.WriteLine("Exiting...");
+                       return;
+                default: Console.WriteLine("Invalid choice. Please choose again.");
+                       break;
+            }
+        }
+   }
+}
+
+
+
+
