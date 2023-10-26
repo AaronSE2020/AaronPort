4dc5a461952c37b6d6e41cb7b73ebb3a3963dac7
dtruth30 <112116697+dtruth30@users.noreply.github.com>
2023-10-25 20:50:35 -0500

    commit

--- TaskManagerProject/Program.cs
+++ TaskManagerProject/Program.cs
@@ -2,66 +2,62 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.ReverseProxy.Abstractions;
+using TaskManagerProject.Data;
+using TaskManagerProject.Services;
+using Microsoft.EntityFrameworkCore;
+using Yarp.ReverseProxy.Configuration;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TaskManagerProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TaskManagerProjectContext") ?? throw new InvalidOperationException("Connection string 'TaskManagerProjectContext' not found.")));
 
-internal class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<TaskManagerService>();
+
+// Configure the reverse proxy
+var proxyBuilder = builder.Services.AddReverseProxy();
+proxyBuilder.LoadFromMemory(new[]
 {
-    private static void Main(string[] args)
+    new RouteConfig
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddDbContext<TaskManagerProjectContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("TaskManagerProjectContext") ?? throw new InvalidOperationException("Connection string 'TaskManagerProjectContext' not found.")));
-
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddTransient<TaskManagerService>();
-
-        // Configure the reverse proxy
-        var proxyBuilder = builder.Services.AddReverseProxy();
-        proxyBuilder.LoadFromMemory(new[]
-        {
-            new ProxyRoute
-            {
-                RouteId = "api-route",
-                ClusterId = "api-cluster",
-                Match = new ProxyMatch { Path = "/api/{**catch-all}" }
-            }
-        }, new[]
-        {
-            new Cluster
-            {
-                Id = "api-cluster",
-                Destinations = new Dictionary<string, Destination>
-                {
-                    ["api-destination"] = new Destination { Address = "https://s-install.avcdn.net/" }
-                }
-            }
-        });
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
+        RouteId = "api-route",
+        ClusterId = "api-cluster",
+        Match = new RouteMatch { Path = "/api/{**catch-all}" }
     }
+}, new[]
+{
+    new ClusterConfig
+    {
+        ClusterId = "api-cluster",
+        Destinations = new Dictionary<string, DestinationConfig>
+        {
+            ["api-destination"] = new DestinationConfig { Address = "https://s-install.avcdn.net/" }
+        }
+    }
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
 
 internal class Destination
 {
